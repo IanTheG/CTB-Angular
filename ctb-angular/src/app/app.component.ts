7f5f5421bb20d8bf206ae91798cc1c7cd620d05b
IanTheG <goodwinij1@gmail.com>
2021-07-13 17:24:05 -0500

    Updated service worker to hopefully include images in caching Updated index.t

--- ctb-angular/src/app/app.component.ts
+++ ctb-angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ctb-angular'
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Connecting The Beads'
+
+  // Watch for window resize changes to change viewport window size
+  setDocHeight() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`)
+  }
+
+  ngOnInit() {
+    window.addEventListener('load', this.setDocHeight)
+    window.addEventListener('resize', this.setDocHeight)
+    window.addEventListener('orientationchange', this.setDocHeight)
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('load', this.setDocHeight)
+    window.removeEventListener('resize', this.setDocHeight)
+    window.removeEventListener('orientationchange', this.setDocHeight)
+  }
 }
